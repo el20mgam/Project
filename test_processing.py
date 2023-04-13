9a3e91401db4a91c42ef49df938a79f8ea3c6e35
Matthew Moore <matthewmoore@Matthews-MBP.lan>
2023-04-13 14:22:26 +0100

    Changed to measureing magenta channel

--- test_processing.py
+++ test_processing.py
@@ -1,12 +1,17 @@
 from PIL import Image
 
-def contains_red(image, region):
+
+def contains_magenta(image, region):
+    # Convert the image to CMYK
+    cmyk_image = image.convert('CMYK')
+
     for x in range(region[0], region[0] + region[2]):
         for y in range(region[1], region[1] + region[3]):
-            r, g, b = image.getpixel((x, y))
-            if r > g and r > b:  # Check if red is the dominant color
+            c, m, y, k = cmyk_image.getpixel((x, y))
+            if m > c and m > y:  # Check if magenta is the dominant color
                 return True
     return False
+
 
 # Load your image
 image_path = 'LYG61973776.png'
@@ -16,9 +21,9 @@ image = Image.open(image_path)
 region1 = (x1, y1, width1, height1)
 region2 = (x2, y2, width2, height2)
 
-# Check for red in the regions
-red_in_region1 = contains_red(image, region1)
-red_in_region2 = contains_red(image, region2)
+# Check for magenta in the regions
+magenta_in_region1 = contains_magenta(image, region1)
+magenta_in_region2 = contains_magenta(image, region2)
 
-print(f"Region 1 contains red: {red_in_region1}")
-print(f"Region 2 contains red: {red_in_region2}")
+print(f"Region 1 contains magenta: {magenta_in_region1}")
+print(f"Region 2 contains magenta: {magenta_in_region2}")
