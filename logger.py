b0dbf948a4947243f202bbfcd668cc19eb4525d9
Matthew Moore <matthewmoore@Matthews-MBP.lan>
2023-04-13 13:34:25 +0100

    Added SQLite db for saving data

--- logger.py
+++ logger.py
@@ -1,36 +1,46 @@
 import time
-import csv
+import sqlite3
 import math
 from scd30_i2c import SCD30
 
-scd30 = SCD30()
+# Connect to the database
+conn = sqlite3.connect('co2_measurements_IOM.db')
+c = conn.cursor()
 
+# Create the table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS measurements
+             (datetime text, co2 real, temperature real, humidity real)''')
+
+scd30 = SCD30()
 scd30.set_measurement_interval(2)
 scd30.start_periodic_measurement()
 scd30.set_temperature_offset(2)
 
 time.sleep(2)
 
-with open('measurements_bedroom.csv', mode='a', newline='') as file:
-    writer = csv.writer(file)
+while True:
+    if scd30.get_data_ready():
+        m = scd30.read_measurement()
+        if m is not None:
+            co2 = m[0] if not math.isnan(m[0]) else 0
+            temp = m[1] if not math.isnan(m[1]) else 0
+            rh = m[2] if not math.isnan(m[2]) else 0
 
-    # Write the header row
-    writer.writerow(['Date Time', 'CO2 (ppm)', 'Temperature (C)', 'Relative Humidity (%)'])
+            # Get the current date and time to the second
+            date_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
-    while True:
-        if scd30.get_data_ready():
-            m = scd30.read_measurement()
-            if m is not None:
-                co2 = m[0] if not math.isnan(m[0]) else 0
-                temp = m[1] if not math.isnan(m[1]) else 0
-                rh = m[2] if not math.isnan(m[2]) else 0
+            print(f"CO2: {co2:.2f}ppm, temp: {temp:.2f}'C, rh: {rh:.2f}%")
 
-                # Get the current date and time to the second
-                date_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+            # Insert the data into the database
+            c.execute("INSERT INTO measurements VALUES (?, ?, ?, ?)", (date_time, co2, temp, rh))
+            conn.commit()
 
-                print(f"CO2: {co2:.2f}ppm, temp: {temp:.2f}'C, rh: {rh:.2f}%")
-                # Write the row with the current date and time in the first column
-                writer.writerow([date_time, co2, temp, rh])
-            time.sleep(2)
-        else:
-            time.sleep(0.2)
+        time.sleep(2)
+
+    else:
+        time.sleep(0.2)
+
+
+# Close the database connection
+c.close()
+conn.close()
