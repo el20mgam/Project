f62b0bbd6f0eeb021437aeb0b1389a56b9183718
Matthew Moore <matthewmoore@Matthews-MBP.lan>
2023-04-13 13:38:27 +0100

    Added time based filter

--- graph.py
+++ graph.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--start', type=str, help='start date in YYYY-MM-DD format')
+parser.add_argument('--end', type=str, help='end date in YYYY-MM-DD format')
+parser.add_argument('--csv_file', type=str, help='name of the CSV file')
+args = parser.parse_args()
 
 # read in the CSV file
-df = pd.read_csv('total.csv')
+df = pd.read_csv(args.csv_file)
 
+# convert the 'Date Time' column to a datetime object
 df['Date Time'] = pd.to_datetime(df['Date Time'])
+
+# filter the data based on the specified date range
+if args.start and args.end:
+    start_date = pd.to_datetime(args.start)
+    end_date = pd.to_datetime(args.end)
+    mask = (df['Date Time'] >= start_date) & (df['Date Time'] <= end_date)
+    df = df.loc[mask]
 
 # create a plot of Date Time against CO2
 plt.plot(df['Date Time'], df['CO2 (ppm)'])
