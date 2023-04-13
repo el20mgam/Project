bc45e521e4a7eac590302eb7e57688297f0eaf03
Matthew Moore <matthewmoore@Matthews-MBP.lan>
2023-04-13 14:23:38 +0100

    Outputs the result of the test and the intensity of the channel

--- test_processing.py
+++ test_processing.py
@@ -5,12 +5,15 @@ def contains_magenta(image, region):
     # Convert the image to CMYK
     cmyk_image = image.convert('CMYK')
 
+    max_magenta_percentage = 0
     for x in range(region[0], region[0] + region[2]):
         for y in range(region[1], region[1] + region[3]):
             c, m, y, k = cmyk_image.getpixel((x, y))
-            if m > c and m > y:  # Check if magenta is the dominant color
-                return True
-    return False
+            magenta_percentage = m / 255 * 100
+            max_magenta_percentage = max(max_magenta_percentage, magenta_percentage)
+            if magenta_percentage > 50:
+                return True, max_magenta_percentage
+    return False, max_magenta_percentage
 
 
 # Load your image
@@ -18,12 +21,20 @@ image_path = 'LYG61973776.png'
 image = Image.open(image_path)
 
 # Define your two regions as (x, y, width, height)
-region1 = (x1, y1, width1, height1)
-region2 = (x2, y2, width2, height2)
+control = (1414, 733, 39, 13)
+test = (1424, 788, 30, 8)
 
-# Check for magenta in the regions
-magenta_in_region1 = contains_magenta(image, region1)
-magenta_in_region2 = contains_magenta(image, region2)
+# Check for magenta above 50% in the regions
+magenta_in_control, control_max_magenta = contains_magenta(image, control)
+magenta_in_test, test_max_magenta = contains_magenta(image, test)
 
-print(f"Region 1 contains magenta: {magenta_in_region1}")
-print(f"Region 2 contains magenta: {magenta_in_region2}")
+# Output results
+if not magenta_in_control and not magenta_in_test:
+    print("Invalid")
+elif not magenta_in_test and magenta_in_control:
+    print("Negative")
+elif magenta_in_test and magenta_in_control:
+    print("Positive")
+
+print(f"Control magenta percentage: {control_max_magenta:.2f}%")
+print(f"Test magenta percentage: {test_max_magenta:.2f}%")
